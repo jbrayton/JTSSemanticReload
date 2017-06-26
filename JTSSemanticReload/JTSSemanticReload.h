13341bb490723b80ab06a8a2259f6482a36732c5
John Brayton <jbrayton@goldenhillsoftware.com>
2017-06-26 15:22:37 -0400

    Carthage-ready Xcode project.

---
+++ JTSSemanticReload/JTSSemanticReload.h
@@ -0,0 +1,19 @@
+//
+//  JTSSemanticReload.h
+//  JTSSemanticReload
+//
+//  Created by John Brayton on 6/26/17.
+//  Copyright © 2017 John Brayton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JTSSemanticReload.
+FOUNDATION_EXPORT double JTSSemanticReloadVersionNumber;
+
+//! Project version string for JTSSemanticReload.
+FOUNDATION_EXPORT const unsigned char JTSSemanticReloadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JTSSemanticReload/PublicHeader.h>
+
+
